0cf2f7464701aa2b6e4a9dd1dd7643b92e764725
rshariffdeen <rshariffdeen@gmail.com>
2020-09-16 13:43:22 +0800

    adding subtraction check

--- instrument/inst_sub_overflow.c
+++ instrument/inst_sub_overflow.c
@@ -133,12 +133,12 @@ static bool option_debug   = true;
 static bool option_disable = true;
 
 /*
- * Safe addition.
+ * Safe subtraction.
  */
-static void safe_add(int64_t s1, int64_t s2, uint16_t *rflags, int64_t lb,
+static void safe_sub(int64_t s1, int64_t s2, uint16_t *rflags, int64_t lb,
                         int64_t ub, const char *asm_str, const void *addr)
 {
-    __int128 d = (__int128)s1 + (__int128)s2;
+    __int128 d = (__int128)s1 - (__int128)s2;
     __int128 c = d;
     bool overflow = (d < lb || d > ub);
 
@@ -147,13 +147,13 @@ static void safe_add(int64_t s1, int64_t s2, uint16_t *rflags, int64_t lb,
         struct stream_s stream_0 = {0};
         stream_t stream = &stream_0;
 
-        write_string(stream, "\33[31mDETECT ADD OVERFLOW\33[0m: \33[32m");
+        write_string(stream, "\33[31mDETECT SUB OVERFLOW\33[0m: \33[32m");
         write_string(stream, asm_str);
         write_string(stream, "\33[0m @ ");
         write_hex(stream, (intptr_t)addr);
         write_string(stream, " (\33[33m");
         write_int(stream, s1);
-        write_string(stream, " + ");
+        write_string(stream, " - ");
         write_int(stream, s2);
         write_string(stream, " = ");
         write_int(stream, (int64_t)c);
@@ -183,22 +183,22 @@ DEBUG=1 ./a.out
 */
 
 /*
- * Safe addition (64bit two operand form)
+ * Safe subtraction (64bit two operand form)
  */
-void add_r64r64(const int64_t *S1, const int64_t *S2, int64_t *D,
+void sub_r64r64(const int64_t *S1, const int64_t *S2, int64_t *D,
                 uint16_t *rflags, const char *asm_str, const void *addr)
 {
-    safe_add(*S1, *S2, rflags, INT64_MIN, INT64_MAX, asm_str, addr);
+    safe_sub(*S1, *S2, rflags, INT64_MIN, INT64_MAX, asm_str, addr);
 }
 
 
 /*
  * Safe addition (32bit two operand form)
  */
-void add_r32r32(const int32_t *S1, const int32_t *S2, int32_t *D,
+void sub_r32r32(const int32_t *S1, const int32_t *S2, int32_t *D,
                 uint16_t *rflags, const char *asm_str, const void *addr)
 {
-    safe_add((int64_t)*S1, (int64_t)*S2, rflags, INT32_MIN,
+    safe_sub((int64_t)*S1, (int64_t)*S2, rflags, INT32_MIN,
                            INT32_MAX, asm_str, addr);
 }
 
